7122f036eaa18a33c4d29e3bbf77a855b61752bb
vasavi vemuru <vasavivemuru@vasavis-MacBook-Pro.local>
2015-08-03 14:05:13 -0700

    Initial Commit

---
+++ NSString Examples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSString Examples
+//
+//  Created by vasavi vemuru on 8/3/15.
+//  Copyright (c) 2015 vasavi vemuru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSString Examples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSString Examples
+//
+//  Created by vasavi vemuru on 8/3/15.
+//  Copyright (c) 2015 vasavi vemuru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
